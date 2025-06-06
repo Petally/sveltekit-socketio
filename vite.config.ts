3a55fe22f966099ab569a9797ecbb13c99a68d5b
petally <petalimium@gmail.com>
2025-06-06 15:55:39 +0300

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { webSocketPlugin } from './vite-plugin-websocket.js';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [sveltekit(), webSocketPlugin()]
+    plugins: [tailwindcss(), sveltekit(), webSocketPlugin()]
 });
